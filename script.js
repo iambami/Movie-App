983b982d233f4c1ac569433d74b5613e733edb26
Atinuke Bami Kayode <titatinuks@gmail.com>
2021-07-08 06:18:40 +0100

    upadated

--- script.js
+++ script.js
@@ -25,17 +25,29 @@ function showMovies(movies) {
         <div class="movie">
             <img src="${IMG_PATH + poster_path}" alt="${title}"/>
             <div class="movie-info">
-            <h3>${title}</h3>  
-            <span class="green">${vote_avarage</span>
+         <h3>${title}</h3>
+         <span class="${getClassByRate (vote_avarage)}">${vote_avarage}</span>
             </div>
             <div class="overview">
-                <h3>Overview</h3>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum cupiditate ab placeat cumque quisquam sint rerum repellat dolor aperiam eaque. 
-            </div>
+         <h3>Overview</h3>
+           ${overview}
         </div>
-          `
-         
+     </div>
+        `
+
+        main.appendChild(movieEl)
+          
     })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
 }
 
 form.addEventListener('submit', (e) => {
